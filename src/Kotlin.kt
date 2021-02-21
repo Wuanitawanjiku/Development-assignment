b308f7d0fdac83e3e891ba9f85cc24db4d0753d2
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-02-21 11:30:06 +0300

    Development assignment

--- src/Kotlin.kt
+++ src/Kotlin.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val name = arrayOf("Joy Jahenda","Rosemary Adera","Faith Mwende","Deborah Mutheu")
-    val age = arrayOf(18,23,19,20)
-    val phoneNumber = arrayOf("0700345345","0711133344","0755548899","0742678567")
-    val weight = arrayOf(40,65,52,43)
-    val citizenship = "True"
+   val name:String="Wuanita Wanjiku"
+   var age =19
+    val phoneNumber="0723498721"
+    var weight =48
+    val citizenship:Boolean=true
 }
